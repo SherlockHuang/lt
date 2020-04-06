f1bacf214864e4562f7158b294d982ccb83f7629
PsCool <hpsmayday@gmail.com>
2020-04-07 00:41:11 +0800

    up test script

--- test_lt.lua
+++ test_lt.lua
@@ -3,49 +3,41 @@ local sf = string.format
 
 local m = collectgarbage "count"
 local t = os.clock()
-pt = p.create_lt('output.pl')
+local pt = p.create_lt('output.pl')
 local dt = os.clock() - t
 local dm = collectgarbage("count") - m
 print(sf('create lt cost %f, used mem:%.1fM', dt, dm / 1024))
 
 collectgarbage "collect"
-collectgarbage "collect"
-collectgarbage "collect"
-local dm = collectgarbage("count") - m
+dm = collectgarbage("count") - m
 print(sf('create lt used mem:%.1fM', dm / 1024))
 
-local m = collectgarbage "count"
+m = collectgarbage "count"
 t = os.clock()
-lt = require 'test_1'
+local lt = loadfile('test_1.lua')()
 dt = os.clock() - t
 dm = collectgarbage("count") - m
 print(sf('create raw cost %f, used mem:%.1fM', dt, dm / 1024))
 
 collectgarbage "collect"
-collectgarbage "collect"
-collectgarbage "collect"
-
 dm = collectgarbage("count") - m
 print(sf('create raw used mem:%.1fM', dm / 1024))
 
 m = collectgarbage "count"
 t = os.clock()
-lc = loadfile('test_1.luac')()
+local lc = loadfile('test_1.luac')()
 dt = os.clock() - t
 dm = collectgarbage("count") - m
 print(sf('create compiled raw cost %f, used mem:%.1fM', dt, dm / 1024))
 
 collectgarbage "collect"
-collectgarbage "collect"
-collectgarbage "collect"
-local dm = collectgarbage("count") - m
+dm = collectgarbage("count") - m
 print(sf('create compiled raw used mem:%.1fM', dm / 1024))
 
 print('---------- ipairs  ----------')
 local tv = 0
 t = os.clock()
 for k, v in ipairs(pt) do
-    -- print('k, v = ', k, v)
     tv = tv + v
 end
 print(sf('ipairs lt table cost %f, tv = %s', os.clock() -t, tv))
@@ -54,7 +46,6 @@ print('---------- ipairs  ----------')
 tv = 0
 t = os.clock()
 for k, v in p.ipairs(pt) do
-    -- print('k, v = ', k, v)
     tv = tv + v
 end
 print(sf('p.ipairs lt table cost %f, tv = %s', os.clock() -t, tv))
@@ -63,11 +54,17 @@ print('---------- ipairs  ----------')
 tv = 0
 t = os.clock()
 for k, v in ipairs(lt) do
-    -- print('k, v = ', k, v)
     tv = tv + v
 end
 print(sf('ipairs raw table cost %f, tv = %s', os.clock() -t, tv))
 
+print('---------- ipairs  ----------')
+tv = 0
+t = os.clock()
+for k, v in ipairs(lc) do
+    tv = tv + v
+end
+print(sf('ipairs compiled raw table cost %f, tv = %s', os.clock() -t, tv))
 
 print('---------- pairs  ----------')
 tv = 0
@@ -92,3 +89,11 @@ for k, v in pairs(lt) do
     tv = tv + v
 end
 print(sf('pairs raw table cost %f, tv = %s', os.clock() -t, tv))
+
+print('---------- pairs  ----------')
+tv = 0
+t = os.clock()
+for k, v in pairs(lc) do
+    tv = tv + v
+end
+print(sf('pairs compiled raw table cost %f, tv = %s', os.clock() -t, tv))
